333706d0dbd6801aef05a8160d15b6a64fd6336a
Ricardo Sixel <rsixel@gmail.com>
2018-02-20 22:38:56 -0300

    Criada a aplicação 'enquetes'

--- home_site/enquetes/models.py
+++ home_site/enquetes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Enquete(models.Model):
+    texto = models.CharField(max_length=250)
+
+
+class Resposta(models.Model):
+    enquete = models.ForeignKey(Enquete, on_delete=models.CASCADE)
+    opcao = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
