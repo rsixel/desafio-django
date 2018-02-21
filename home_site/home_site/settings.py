3eae6586e8a7dddc645e3feb80b40c5ec9c422db
Ricardo Sixel <rsixel@gmail.com>
2018-02-20 22:46:04 -0300

    Criado o modelo no banco

--- home_site/home_site/settings.py
+++ home_site/home_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'enquetes.apps.EnquetesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
