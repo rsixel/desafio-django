2096d17e360dcf05fcce06d0a8b9c05232bb75e3
Ricardo Sixel <rsixel@gmail.com>
2018-02-28 21:26:02 -0300

    BONUS Configuração com honcho do web e worker simultâneos no mesmo Dynos

--- heroku_deploy_celery.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/sh
-
-cp Procfile_celery Procfile
-
-git commit -am "Procfile"
-git push heroku master
-
-
-heroku ps:scale worker=1
--- heroku_deploy_django.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/sh
-
-cp Procfile_web Procfile
-
-git commit -am "Procfile"
-git push heroku master
-
-
-heroku ps:scale web=1
